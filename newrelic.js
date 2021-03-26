63f77443024fa4e2e9d8d6be83bdf9db476f0ec6
Terri Regan <7433075+terriregan@users.noreply.github.com>
2021-03-26 16:27:05 -0400

    fix: new relic integration

--- newrelic.js
+++ newrelic.js
@@ -1,13 +1,13 @@
+require('dotenv').config();
+
 /**
  * New Relic agent configuration.
  *
  * See lib/config/default.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
-exports.config = {
-  agent_enabled:
-    ( ( process.env.NEW_RELIC_LICENSE_KEY && process.env.NEW_RELIC_LICENSE_KEY !== '' ) || null )
-    !== null,
+exports.config = { 
+  agent_enabled: !!( process.env.REACT_NEW_RELIC_LICENSE_KEY && process.env.REACT_NEW_RELIC_LICENSE_KEY !== '' ),
   /**
    * Array of application names.
    */
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import {} from 'dotenv/config';
 import http from 'http';
 import app from './server';
 
-require( 'newrelic' );
-
 // Used for module hot reloading, will maintain state
 const server = http.createServer( app );
 const PORT = process.env.PORT || 8080;
